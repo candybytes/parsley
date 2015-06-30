e33ca86c44bdc318180ed88ac02edb332342dd51
Danish Markatia <dmarkatia@aol.com>
2015-06-30 17:23:22 -0400

    Revert "06/30/2015"
    This reverts commit eb5d5af12896f9dd1d4d250e15440972ee6870eb.

--- parser.c
+++ parser.c
@@ -23,32 +23,32 @@ int inputTokens = 0;
 
 // -----------------Initial call to program  -----------------
 int main(int argc, char *argv[]) {
-
+    
     FILE *ifp = NULL;
     ifp = fopen("lexemelist.txt", "r");
     if (ifp == NULL) {
         printf("Need to add error number for file open here\n");
     }
-
+    
     // will do linked list of char* to store each token without knowing count of tokens
     // and have a local-global token counter
     // all tokens no matter what, will be max length of 12
     char iToken[MAX_VAR_LEN + 1];  // One extra for nul char. //
-
+    
 
     while (fscanf(ifp, "%s", iToken) != EOF) {
         // instead of printf, just create a new node of linked list with string
         printf("%s\n", iToken);
         inputTokens++;
-
+        
     }
     printf("%d tokens \n", inputTokens);
 
     // close the input file
     fclose(ifp);
+    
 
-
-
-
+    
+    
     return 0;
 }
